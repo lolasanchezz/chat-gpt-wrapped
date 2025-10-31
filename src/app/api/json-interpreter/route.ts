396d72c825c94074274cfb540ef7898b7b4493ab
lolasanchezz <lolansanchez@icloud.com>
2025-10-31 12:00:07 -0400

    decoding js

--- src/app/api/json-interpreter/route.ts
+++ src/app/api/json-interpreter/route.ts
@@ -1,25 +1,54 @@
 import { NextRequest, NextResponse } from "next/server";
 
- interface returnedData {
-    avgConvoStartTime?: number;
-    avgPromptLen?: number;
-    avgConvoLen?: number;
-    avgConvoTimeLength?: number;
-    error?: string
-  }
+interface returnedData {
+  avgConvoStartTime?: number;
+  avgPromptLen?: number;
+  avgConvoLen?: number;
+  avgConvoTimeLength?: number;
+  error?: string;
+}
 
 export const POST = async (req: NextRequest) => {
-
-    let testObj: returnedData = {
+  let testObj: returnedData = {
     avgConvoStartTime: 0,
     avgPromptLen: 0,
     avgConvoLen: 0,
     avgConvoTimeLength: 0,
-    error: ""
+    error: "",
+  };
+
+  const formData = await req.formData();
+  const body = Object.fromEntries(formData);
+  const file = (body.file as Blob) || null;
+
+  if (file) {
+    const buffer = Buffer.from(await file.arrayBuffer());
+  } else {
+    const err: returnedData = {
+      error: "couldn't read file",
+    };
+    return NextResponse.json({
+      success: false,
+      body: err,
+    });
   }
+
+  let jsonBody = ""
+  await file.text().then((txt) => { jsonBody = txt })
+  jsonBody = JSON.parse(jsonBody)
+
+
+
+
+
 
   return NextResponse.json({
     success: true,
-    body: JSON.stringify(testObj)
+    body: JSON.stringify(testObj),
   });
+};
+
+
+function getAvgConvLen(jsBody: any) {
+
 }
